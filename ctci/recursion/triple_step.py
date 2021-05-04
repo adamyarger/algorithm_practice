e8e38036601910cb4ef6e4fbcf2b78310ccad4ed
Adam Yarger <yargeradam@gmail.com>
2021-05-04 11:06:37 -0700

    adds fibonacci steps

--- ctci/recursion/triple_step.py
+++ ctci/recursion/triple_step.py
@@ -13,14 +13,34 @@
 # base case if we hit 0 there only one way and thats to do nothing
 
 # think of the simple cases that you know first, these are most likely base cases
-
-def triple_step(steps):
-    # why 0?
+def slow_triple_step(steps):
     if steps == 0 or steps == 1:
         return 1
     if steps == 2:
         return 2
-    return triple_step(steps-3) + triple_step(steps-2) + triple_step(steps-1)
+    return slow_triple_step(steps-3) + slow_triple_step(steps-2) + slow_triple_step(steps-1)
 
 
-print(triple_step(4))
+def triple_step(steps):
+    return _triple_step(steps, [-1]*(steps+1))
+
+
+def _triple_step(x, memo):
+    if x < 0:
+        return 0
+    # we know for sure if the amount of steps is 0 then there is only one way, so we set that here
+    memo[0] = 1
+    # we need to fill in the first 3 items in the memo array since they represent the 3 first step ways
+    # we then use those ways to calculate the next step, this is the same as fibonacci but with 3 items bing added together
+    if x >= 1:
+        memo[1] = 1
+    if x >= 2:
+        memo[2] = memo[1] + memo[0]
+    if x > 2:
+        # 3 is for the variation of steps
+        for i in range(3, x + 1):
+            memo[i] = memo[i-1] + memo[i-2] + memo[i-3]
+    return memo[x]
+
+
+print(triple_step(50))
