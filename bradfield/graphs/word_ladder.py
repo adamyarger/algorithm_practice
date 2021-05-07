c0b59fadb9a1422e9340df387f21c34166a98975
Adam Yarger <yargeradam@gmail.com>
2021-05-07 12:51:17 -0700

    adds word ladder

--- bradfield/graphs/word_ladder.py
+++ bradfield/graphs/word_ladder.py
@@ -1,8 +1,24 @@
 '''
 * to create a adjacency list, take the start word "pope" and create a bucket for each version of the word with one char missing. _ope, p_op
+
+representing a graph as a dict
+* the key 0 is the vertex (node), the nested dicts keys are the nodes it connects to, this creates the edges (0->1, 0->5)
+* the value in the nested dict is the weight of the edge, so (0->1) has a weight of 5
+{
+    0: {1: 5, 5: 2},
+    1: {2: 4},
+    2: {3: 9},
+    3: {4: 7, 5: 3},
+    4: {0: 1},
+    5: {4: 8}
+}
+
+* to navigate the graph were going to use breadth first search
+* BFS goes layer by layer, it also uses a **queue** to keep track of what to explore next,
+  we put the current nodes children in a queue
 '''
 
-from collections import defaultdict
+from collections import defaultdict, deque
 from itertools import product
 import os
 
@@ -38,8 +54,28 @@ def get_words(vocabulary_file):
         yield line[:-1]
 
 
+def traverse(graph, starting_vertex):
+    visited = set()
+    # dequeue allows O(1) for poping from the front of the array
+    # why nested list? because the nested list represents the neighbors of each node?
+    queue = deque([[starting_vertex]])
+    while queue:
+        # path is an array under a dict key like _OOL
+        path = queue.popleft()
+        vertex = path[-1]
+        yield vertex, path
+        for neighbor in graph[vertex] - visited:
+            visited.add(neighbor)
+            queue.append(path + [neighbor])
+
+
 if __name__ == '__main__':
     vocabulary_file = os.path.join(
-        os.path.dirname(__file__), 'vocabulary_simple.txt')
+        os.path.dirname(__file__), 'vocabulary.txt')
     word_graph = build_graph(get_words(vocabulary_file))
-    print(word_graph)
+    # print(word_graph)
+
+    for vertex, path in traverse(word_graph, 'FOOL'):
+        if vertex == 'SAGE':
+            print(' -> '.join(path))
+            # FOOL -> FOOD -> FOLD -> SOLD -> SOLE -> SALE -> SAGE
