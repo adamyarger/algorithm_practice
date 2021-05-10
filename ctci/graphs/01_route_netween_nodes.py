e762e1e75863afc048c854947761ef494282455d
Adam Yarger <yargeradam@gmail.com>
2021-05-10 14:16:54 -0700

    adds dfs without nested function

--- ctci/graphs/01_route_netween_nodes.py
+++ ctci/graphs/01_route_netween_nodes.py
@@ -31,7 +31,26 @@ def dfs(graph, start, end):
     return found[0]
 
 
-print(dfs(simple_graph, 'A', 'E'))  # True
-print(dfs(simple_graph, 'A', 'B'))  # True
-print(dfs(simple_graph, 'A', 'C'))  # True
-print(dfs(simple_graph, 'A', 'R'))  # False
+# we need visited, else the visited set gets reset each time the function is called
+def is_route(graph, start, end, visited=None):
+    if visited is None:
+        visited = set()
+    for node in graph[start]:
+        if node not in visited:
+            visited.add(node)
+            # the key here is calling is_route again and if its already found
+            # the mach to keep returning True till the stack is empty
+            if node == end or is_route(graph, node, end, visited):
+                return True
+    return False
+
+
+# print(dfs(simple_graph, 'A', 'E'))  # True
+# print(dfs(simple_graph, 'A', 'B'))  # True
+# print(dfs(simple_graph, 'A', 'C'))  # True
+# print(dfs(simple_graph, 'A', 'R'))  # False
+
+print(is_route(simple_graph, 'A', 'E'))  # True
+print(is_route(simple_graph, 'A', 'B'))  # True
+print(is_route(simple_graph, 'A', 'C'))  # True
+print(is_route(simple_graph, 'A', 'R'))  # False
