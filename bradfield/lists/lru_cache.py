03fbaedc7ceee3d4556af113f17b75d8fefd1634
Adam Yarger <yargeradam@gmail.com>
2021-05-22 14:15:06 -0700

    adds comments to lru

--- bradfield/lists/lru_cache.py
+++ bradfield/lists/lru_cache.py
@@ -31,6 +31,12 @@ class LRUCache:
         self.head.next = self.tail
         self.tail.prev = self.head
 
+    def loop(self):
+        cur = self.head
+        for _ in range(self.size + 2):
+            print({'key': cur.key, 'value': cur.value})
+            cur = cur.next
+
     def get(self, key):
         if key in self.map:
             # the map has a reference to the node in he linked list
@@ -47,26 +53,36 @@ class LRUCache:
             self._remove(self.map[key])
         node = Node(key, value)
         self._add(node)
-        self.map[key] = node
         if len(self.map) > self.size:
             # remove oldest
             node = self.head.next
             self._remove(node)
-            del self.map[node.key]
 
     def _remove(self, node):
         prev = node.prev
         nxt = node.next
         prev.next = nxt
         nxt.prev = prev
+        del self.map[node.key]
 
     def _add(self, node):
-        # prev is the head
+        '''
+        WE NEVER REPLACE HEAD AND TAIL!!!
+        THEY ARE JUST BUFFERS.
+        '''
+        # new nodes prev will point here
+        print(self.tail.prev.key, self.head.key, self.tail.key)
         prev = self.tail.prev
+        # prevs next points to the new node
         prev.next = node
+        # the tail points to the new node which is the new head
         self.tail.prev = node
+        # new nodes prev is the old head
         node.prev = prev
+        # new nodes next is the tail
         node.next = self.tail
+        # add it to the map
+        self.map[node.key] = node
 
 
 cache = LRUCache(5)
@@ -75,4 +91,36 @@ cache.set('b', 'cool b')
 cache.set('c', 'cool c')
 cache.set('d', 'cool d')
 cache.set('e', 'cool e')
-print(cache.get('d'))
+
+# cache.loop()
+
+print(cache.get('a'))
+# cache.loop()
+
+cache.set('f', 'cool f')
+# cache.loop()
+
+print(cache.get('b'))
+
+# {'value': 0, 'key': 0}
+# {'value': 'cool a', 'key': 'a'}
+# {'value': 'cool b', 'key': 'b'}
+# {'value': 'cool c', 'key': 'c'}
+# {'value': 'cool d', 'key': 'd'}
+# {'value': 'cool e', 'key': 'e'}
+# {'value': 0, 'key': 0}
+# cool a
+# {'value': 0, 'key': 0}
+# {'value': 'cool b', 'key': 'b'}
+# {'value': 'cool c', 'key': 'c'}
+# {'value': 'cool d', 'key': 'd'}
+# {'value': 'cool e', 'key': 'e'}
+# {'value': 'cool a', 'key': 'a'}
+# {'value': 0, 'key': 0}
+# {'value': 0, 'key': 0}
+# {'value': 'cool c', 'key': 'c'}
+# {'value': 'cool d', 'key': 'd'}
+# {'value': 'cool e', 'key': 'e'}
+# {'value': 'cool a', 'key': 'a'}
+# {'value': 'cool f', 'key': 'f'}
+# {'value': 0, 'key': 0}
