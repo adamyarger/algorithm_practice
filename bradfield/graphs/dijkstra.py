70bf0dd0b790ff6ff6406b5148d4ee9d6a870b4a
Adam Yarger <yargeradam@gmail.com>
2021-05-28 12:27:31 -0700

    does dijkstra

--- bradfield/graphs/dijkstra.py
+++ bradfield/graphs/dijkstra.py
@@ -3,37 +3,32 @@
 - returns the shortest distance to each node, not the path
 - min heap with a breadth first search
 
+Whats the diffeerence between prims and dijstras?
+
+dijkstra is the shortest distance to a certain node.
+
+prims is the least amount of overall work to visit all nodes.
 '''
 import heapq
 
 
 def calculate_distances(graph, starting_vertex):
-    # set each vertex default value to infinity {'U': inf, 'W': inf, 'V': inf, 'Y': inf, 'X': 0, 'Z': inf}
-    distances = {vertex: float('infinity') for vertex in graph}
-    # set the starting vertex {'X': 0}
+    distances = {node: float('infinity') for node in graph}
     distances[starting_vertex] = 0
-
-    # min heap, we want the shortest vertices at the top, since shorter vertex means shorter path
-    # even if the vertex is large like 8, its still smaller than infinity which is what all the unvisited  apths are
-    # add tuple with weight and where it came from i.e. X
     pq = [(0, starting_vertex)]
+
     while len(pq) > 0:
-        current_distance, current_vertex = heapq.heappop(pq)
-        # do breadth first search for the current vertex, we then add the vertexes children the the prioity queue which will bubble up the shortest path the use next
+        current_distance, current_node = heapq.heappop(pq)
 
-        if current_distance > distances[current_vertex]:
-            continue
+        # is this needed?
+        # if current_distance > distances[current_node]:
+        #     continue
 
-        # visit the current vertexes children
-        # [('Y', 1), ('U', 1), ('W', 3), ('V', 2)]
-        for neighbor, weight in graph[current_vertex].items():
-            # distance will get aggragated, take the vurrent vertex weight and add the neighbors weight
-            distance = current_distance + weight
-
-            # only update the vertexes distance if its better, any time its first visted will be better since it starts at infinity but once it come up again it only updates if its smaller
-            if distance < distances[neighbor]:
-                distances[neighbor] = distance
-                heapq.heappush(pq, (distance, neighbor))
+        for neighbor, distance in graph[current_node].items():
+            total_distance = current_distance + distance
+            if total_distance < distances[neighbor]:
+                heapq.heappush(pq, (total_distance, neighbor))
+                distances[neighbor] = total_distance
     return distances
 
 
