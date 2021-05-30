0bc9128d68368aae23030b9b8b38840ead06b571
Adam Yarger <yargeradam@gmail.com>
2021-05-30 14:50:35 -0700

    adds two sum

--- leetcode/two_pointers/001_two_sum.py
+++ leetcode/two_pointers/001_two_sum.py
@@ -21,3 +21,9 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
+        num_map = {}
+        for index, num in enumerate(nums):
+            if (target - num) in num_map:
+                return [num_map[target-num], index]
+            num_map[num] = index
+        return [-1, -1]
