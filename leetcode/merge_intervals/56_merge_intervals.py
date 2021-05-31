5659cc97006c30e0fd5fc19e234c1d0387ee9117
Adam Yarger <yargeradam@gmail.com>
2021-05-31 13:25:51 -0700

    adds merge interval

--- leetcode/merge_intervals/56_merge_intervals.py
+++ leetcode/merge_intervals/56_merge_intervals.py
@@ -22,34 +22,23 @@ N for for loop
 
 class Solution:
     def merge(self, intervals):
-        # if theres not enough intervals we cant do anythign
         if len(intervals) < 2:
             return intervals
 
-        # we need them to be inorder based on the start value
-        # KEY question to ask in interview, are the intervals sorted?
         intervals.sort(key=lambda x: x[0])
-
-        merged = []
-        # start and end of the first interval, we wont include this in the loop but still need to compare it
-        # first interval
         start = intervals[0][0]
-        # second interval, were checking to see if it can be merged
         end = intervals[0][1]
-
-        # where skipping the first interval in the loop since we need to compare the next one
-        for i in range(1, len(intervals)):
-            interval = intervals[i]
-            # whats going on here?
-            # if the start of the next interval is less than the end of the last one we have an overlap
+        merged = []
+        for index in range(1, len(intervals)):
+            interval = intervals[index]
+            # got this part worng, need to compare the start to see if theres an overlap
             if interval[0] <= end:
-                # create new end, this comsumes the other interval into a new one
-                end = max(interval[1], end)
+                end = max(end, interval[1])
             else:
-                # no overlap, we can add the previous interval
+                # else the interval should stay the same, no new overlap
                 merged.append([start, end])
-                # the current interval becomes the prev we want to compare against
                 start = interval[0]
                 end = interval[1]
-        merged.append([start, end])  # add the last interval
+        # need to add the last one, the loop will have only expanded the interval and not appended it
+        merged.append([start, end])
         return merged
