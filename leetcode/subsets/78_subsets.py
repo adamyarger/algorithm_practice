9ca056e3b6b05524c99a6a1a1dcb8fe7fecb6744
Adam Yarger <yargeradam@gmail.com>
2021-06-01 13:33:44 -0700

    adds subsets

---
+++ leetcode/subsets/78_subsets.py
@@ -0,0 +1,28 @@
+'''
+LeetCode 78 - Subsets [medium]
+
+Given a set of distinct integers, nums, return all possible subsets (the power set).
+
+Note: The solution set must not contain duplicate subsets.
+
+Example:
+
+Input: nums = [1, 2, 3]
+Output:
+[
+  [3],
+  [1],
+  [2],
+  [1, 2, 3],
+  [1, 3],
+  [2, 3],
+  [1, 2],
+  []
+]
+'''
+from typing import List
+
+
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        pass
