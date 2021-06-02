6524b593da9cfe3e595746ad032c92e01d763081
Adam Yarger <yargeradam@gmail.com>
2021-06-02 15:31:05 -0700

    adds top k

--- leetcode/top_k/215_kth_largest_element.py
+++ leetcode/top_k/215_kth_largest_element.py
@@ -13,10 +13,27 @@ Example 2:
 
 Input: nums = [3,2,3,1,2,4,5,5,6], k = 4
 Output: 4
+
+Time Complexity: O(N log K)
+Space Complexity: O(K)
 '''
 from typing import List
+from heapq import *
 
 
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        pass
+        min_heap = []
+
+        # fill up the heap with k elements
+        # by using a min heap with the top k largest items, the kth largest is alwasys at the top
+        for i in range(k):
+            heappush(min_heap, nums[i])
+
+        # we already looped over the first k, let continue the loop
+        for i in range(k, len(nums)):
+            if nums[i] > min_heap[0]:
+                heappop(min_heap)
+                heappush(min_heap, nums[i])
+
+        return min_heap[0]
