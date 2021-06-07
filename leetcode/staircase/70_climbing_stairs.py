f6eaddaca79f217fa25208afa29b495bfd2caf5a
Adam Yarger <yargeradam@gmail.com>
2021-06-07 13:39:11 -0700

    adds memory efficient staircase solution

--- leetcode/staircase/70_climbing_stairs.py
+++ leetcode/staircase/70_climbing_stairs.py
@@ -65,5 +65,36 @@ class Solution:
         return memo[n]
 
 
-sol = Solution()
-print(sol.climbStairs(4))
+# sol = Solution()
+# print(sol.climbStairs(4))
+
+
+def bottomUpClimb(n: int) -> int:
+    tab = [0] * (n+1)
+    # we dont take any steps, so theres only one way
+    tab[0] = 1
+    # we take one step to reach the end, this is the only way
+    tab[1] = 1
+
+    # we took care of the first 2, so skip them in the for loop
+    for i in range(2, n+1):
+        # this is looks like fibonacci
+        tab[i] = tab[i-1] + tab[i-2]
+
+    return tab[n]
+
+
+# print(bottomUpClimb(4))
+
+def bottomUpBetter(n: int) -> int:
+    '''
+    - we only need the last 2 to calculate the next, that means we
+      dont need to store the rest of them and we can save on memory
+    '''
+    a = b = 1
+    for _ in range(n):
+        a, b = b, a + b
+    return a
+
+
+print(bottomUpBetter(4))
