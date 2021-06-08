9620fbe06fdb418e1b56b2a4507dfff3a3d13119
Adam Yarger <yargeradam@gmail.com>
2021-06-08 09:16:34 -0700

    adds subsets

--- leetcode/subsets/78_subsets.py
+++ leetcode/subsets/78_subsets.py
@@ -29,21 +29,47 @@ N grows each iteration
 from typing import List
 
 
+# do this recurisivly as well
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         '''
-        - start with an empty set
-        - copy all existsing items and add the new number to each
+        - start witth an empty array like so: [[]]
+        - iterate through each number [1,2,3], make a copy of the existing subarrays, then add the new number to each in the copy
+        - be sure to use len and range for the loop, other wise well get stuf in a loop
         '''
         subsets = [[]]
-        # this one loops through the nums array
         for num in nums:
-            # this one loops through the substes array, each item is still an array (subset)
-            # must use range otherwise well get stuck in a loop
-            for i in range(len(subsets)):
-                subsets.append([num] + subsets[i])
+            for index in range(len(subsets)):
+                subsets.append(subsets[index] + [num])
         return subsets
+
+    def recurSubsets(self, nums: List[int]) -> List[List[int]]:
+        '''
+        - get all subset at each length
+        - subsets at 0 []
+        subsets at 1 [1][2][3]
+        subsets at 2 [1,2][2,3][1,3]
+        subsets at 3 [1,2,3]
+        '''
+        output = []
+        max_size = len(nums)
+
+        def backtrack(size, first=0, curr=[]):
+            # were making all combination at the current level, so check if its done
+            if len(curr) == size:
+                # when its at size, make a copy and append it to the output
+                output.append(curr[:])
+                return
+            for i in range(first, max_size):
+                curr.append(nums[i])
+                backtrack(size, i + 1, curr)
+                curr.pop()
+
+        for size in range(max_size + 1):
+            backtrack(size)
+        return output
 
 
 sol = Solution()
-print(sol.subsets([1, 2, 3]))
+# print(sol.subsets([1, 2, 3]))
+print(sol.recurSubsets([1, 2, 3]))
