e78a17419612d8af8fadf6d8a4214453c7a3b04a
Adam Yarger <yargeradam@gmail.com>
2021-06-21 13:36:15 -0700

    adds bottom up palindrome

--- leetcode/palindromes/516_longest_palindromic_subsequence.py
+++ leetcode/palindromes/516_longest_palindromic_subsequence.py
@@ -14,6 +14,10 @@ Example 2:
 Input: s = "cbbd"
 Output: 2
 Explanation: One possible longest palindromic subsequence is "bb".
+
+Time Complexity: O(N2) because memoization array, memo[len(s)][len(s)]. We will not have more than N*N subsequences.
+
+Space Complexity: O(N2 + N) == O(N2) because we used N2 for memoization array and N for recursive stack.
 '''
 
 
@@ -40,7 +44,7 @@ class Solution:
 
         if memo[start][end] == -1:
             if s[start] == s[end]:
-                # since both on the outside match we can bring them both closer together to see if their is still symitry
+                # since both on the outside match we can bring them both closer together to see if their is still symmetry
                 memo[start][end] = 2 + self.recur(memo, s, start + 1, end - 1)
             else:
                 sub1 = self.recur(memo, s, start + 1, end)
@@ -50,5 +54,32 @@ class Solution:
         return memo[start][end]
 
 
-sol = Solution()
-print(sol.longestPalindromeSubseq('bbbab'))
+# sol = Solution()
+# print(sol.longestPalindromeSubseq('bbbab'))
+
+
+class BU:
+    def bottom_up(self, s: str) -> int:
+        size = len(s)
+        memo = [[0] * size for _ in range(size)]
+
+        # every sequence with 1 letter is a palindrom eof size 1
+        for i in range(len(s)):
+            memo[i][i] = 1
+
+        # range in reverse order
+        for start in range(size - 1, -1, -1):
+            # range in ascending order
+            for end in range(start + 1, size):
+                if s[start] == s[end]:
+                    # start and end are the same letter add 2
+                    memo[start][end] = 2 + memo[start + 1][end - 1]
+                else:
+                    # skip one letter either from the beginning or end
+                    memo[start][end] = max(
+                        memo[start + 1][end], memo[start][end - 1])
+        return memo[0][size - 1]
+
+
+bu = BU()
+print(bu.bottom_up('bbbab'))
