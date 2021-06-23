7c71a8fa2af2f874fe88cb2b9b0df6a6f50b9aef
Adam Yarger <yargeradam@gmail.com>
2021-06-23 11:50:37 -0700

    adds lisding window

--- leetcode/sliding_window/003_longest_substring_without_repeating_characters.py
+++ leetcode/sliding_window/003_longest_substring_without_repeating_characters.py
@@ -17,17 +17,26 @@ class Solution(object):
         """
         start = 0
         max_length = 0
+        # char as key and its index as the value
         used = {}
 
         for index, char in enumerate(s):
+            # char has been seen before, (we cant have repeating characters)
+            # char get over written when a new one is seen
+
+            # we saw a repeat, reset the window start
+            # what does this do start <= used[char]??? if our start is already ahead then just ignore it
             if char in used and start <= used[char]:
                 start = used[char] + 1
             else:
+                # is something we saw before bigger i.e. max_lngth... or have we found a bigger substring?
                 max_length = max(max_length, index - start + 1)
+            # always add the newest seen char and its index
             used[char] = index
         return max_length
 
 
 solution = Solution()
 
-print(solution.lengthOfLongestSubstring('abcabcbb'))
+# print(solution.lengthOfLongestSubstring('abcabcbb'))
+print(solution.lengthOfLongestSubstring("tmmzuxt"))  # => 5
