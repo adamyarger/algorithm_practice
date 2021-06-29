cc76dca106bc57aee3f4142897c3cb1088f982a4
Adam Yarger <yargeradam@gmail.com>
2021-06-29 16:40:09 -0700

    adds flatten

---
+++ js/flatten.js
@@ -0,0 +1,20 @@
+function flatten(arr) {
+  return arr.reduce((acc, next) => {
+    const isArray = Array.isArray(next)
+    return acc.concat(isArray ? flatten(next) : next)
+  }, [])
+}
+
+// make it available as an array method.
+if (!Array.prototype.flatten) {
+  Array.prototype.flatten = function () {
+    // need to return this for chaining
+    console.log(this)
+    return flatten(this)
+  }
+}
+
+let arr = [1, 2, [3, 4, [5, 6, [7, [8, 9, 10]]]]]
+// console.log(flatten(arr))
+
+console.log(arr.flatten())
