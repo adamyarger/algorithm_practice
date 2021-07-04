17840b7500ec861ef8d1e3889d70cea74583b953
Adam Yarger <yargeradam@gmail.com>
2021-07-04 14:29:03 -0500

    simplifies deepClone

--- js/deepClone.js
+++ js/deepClone.js
@@ -10,29 +10,27 @@
  * @returns 
  */
 function deepClone(source) {
-  // If the source isn't an Object or Array, throw an error.
-  if (!(source instanceof Object) || source instanceof Date || source instanceof String) {
-    throw 'Only Objects or Arrays are supported.'
+  if (!isCloneable(source)) {
+    throw new Error('Only Objects or Arrays are supported.')
   }
 
-  // Set the target data type before copying.
-  var target = source instanceof Array ? [] : {};
+  var target = Array.isArray(source) ? [] : {}
 
   for (let prop in source) {
-    // Make sure the property isn't on the protoype
-    if (source instanceof Object && !(source instanceof Array) && !(source.hasOwnProperty(prop))) {
-      continue;
-    }
-
-    // If the current property is an Array or Object, recursively clone it, else copy it's value
-    if (source[prop] instanceof Object && !(source[prop] instanceof Date) && !(source[prop] instanceof String)) {
-      target[prop] = deepClone(source[prop])
-    } else {
-      target[prop] = source[prop]
+    if (source.hasOwnProperty(prop)) {
+      if (isCloneable(source[prop])) {
+        target[prop] = deepClone(source[prop])
+      } else {
+        target[prop] = source[prop]
+      }
     }
   }
 
   return target;
+}
+
+function isCloneable(val) {
+  return Array.isArray(val) || Object.prototype.toString.call(val) === '[object Object]'
 }
 
 
