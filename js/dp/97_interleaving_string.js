3125d3abed455813c10b805f57b6f15bf83593c4
Adam Yarger <yargeradam@gmail.com>
2021-07-09 16:24:34 -0500

    started interlaving bottom up

--- js/dp/97_interleaving_string.js
+++ js/dp/97_interleaving_string.js
@@ -85,3 +85,58 @@ s1 = "aabcc"
 s2 = "dbbca"
 s3 = "aadbbbaccc"
 console.log(isInterleave(s1, s2, s3))
+
+
+function bottommUp(s1, s2, s3) {
+  if (s3.length !== s1.length + s2.length) return false
+
+  const cols = s1.length + 1
+  const rows = s2.length + 1
+
+  const memo = Array(cols).fill(false).map(_ => Array(rows).fill(false))
+
+  for (let row = 0; row < cols; row++) {
+    for (let col = 0; col < rows; col++) {
+      if (row == 0 && col == 0) {
+        memo[row][col] = true
+      } else if (row == 0) {
+
+      }
+    }
+  }
+}
+
+
+s1 = "aabcc"
+s2 = "dbbca"
+s3 = "aadbbcbcac"
+console.log(bottommUp(s1, s2, s3))
+
+// s1 = "aabcc"
+// s2 = "dbbca"
+// s3 = "aadbbbaccc"
+// console.log(bottommUp(s1, s2, s3))
+
+
+// public class Solution {
+//   public boolean isInterleave(String s1, String s2, String s3) {
+//     if (s3.length() != s1.length() + s2.length()) {
+//       return false;
+//     }
+//     boolean dp[][] = new boolean[s1.length() + 1][s2.length() + 1];
+//     for (int i = 0; i <= s1.length(); i++) {
+//       for (int j = 0; j <= s2.length(); j++) {
+//         if (i == 0 && j == 0) {
+//           dp[i][j] = true;
+//         } else if (i == 0) {
+//           dp[i][j] = dp[i][j - 1] && s2.charAt(j - 1) == s3.charAt(i + j - 1);
+//         } else if (j == 0) {
+//           dp[i][j] = dp[i - 1][j] && s1.charAt(i - 1) == s3.charAt(i + j - 1);
+//         } else {
+//           dp[i][j] = (dp[i - 1][j] && s1.charAt(i - 1) == s3.charAt(i + j - 1)) || (dp[i][j - 1] && s2.charAt(j - 1) == s3.charAt(i + j - 1));
+//         }
+//       }
+//     }
+//     return dp[s1.length()][s2.length()];
+//   }
+// }
