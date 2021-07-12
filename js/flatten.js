64c263987cf090375abb1dd46015468b6b6e7631
Adam Yarger <yargeradam@gmail.com>
2021-07-12 16:09:24 -0500

    add depth options to flat

--- js/flatten.js
+++ js/flatten.js
@@ -1,19 +1,24 @@
-function flatten(arr) {
+function flat(arr, depth = 1) {
+  if (depth === 0) return arr
   return arr.reduce((acc, item) => {
-    const next = Array.isArray(item) ? flatten(item) : item
+    const next = Array.isArray(item) ? flat(item, depth - 1) : item
     return acc.concat(next)
   }, [])
 }
 
-if (!Array.prototype.flatten) {
-  Array.prototype.flatten = function () {
-    return flatten(this)
+if (!Array.prototype.flat) {
+  Array.prototype.flat = function () {
+    return flat(this)
   }
 }
 
 let arr = [1, 2, [3, 4, [5, 6, [7, [8, 9, 10]]]]]
-// console.log(flatten(arr))
+let arr2 = [1, [2], [3, [4]]]
+// console.log(flat(arr))
 
 // rule of prototypes: the object before the .fn is the this value
 // i.e. the array is this
-console.log(arr.flatten())
+// console.log(arr.flat())
+// console.log(arr2.flat())
+
+console.log(flat(arr2, 1))
