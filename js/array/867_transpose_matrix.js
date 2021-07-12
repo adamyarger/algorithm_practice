3e2c3e5d102cb3bcb0b9299f13609632908822f9
Adam Yarger <yargeradam@gmail.com>
2021-07-12 12:40:26 -0500

    adds transpose

--- js/array/867_transpose_matrix.js
+++ js/array/867_transpose_matrix.js
@@ -20,7 +20,7 @@ Output: [[1,4],[2,5],[3,6]]
 /**
  * @param {number[][]} matrix
  * @return {number[][]}
- */
+ */w
 var transpose = function (matrix) {
   return matrix[0].map((val, index) => matrix.map(row => row[index]))
 };
