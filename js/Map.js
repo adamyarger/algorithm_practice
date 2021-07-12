5b5aa85821a7128f9c8311a7a61574181be0f507
Adam Yarger <yargeradam@gmail.com>
2021-07-11 21:51:41 -0500

    adds map

--- js/Map.js
+++ js/Map.js
@@ -1,4 +1,26 @@
 /**
  * store DOM elements as keys
  * es6 Map type allows this so the gaol is to build Map
+ * 
+ * can use symbol as key... but that doesnt work in es5
+ * 
+ * When to use Symbols? How do they work?
  */
+
+function Map() {
+  const KEY = '__index'
+  const nodeList = []
+  const valueList = []
+
+  this.set = function (key, val) {
+
+  }
+
+  this.get = function (key) {
+
+  }
+
+  this.has = function (key) {
+
+  }
+}
--- js/prototypes/index.js
+++ js/prototypes/index.js
@@ -1,15 +1,52 @@
 
-class Pro {
-  contructor() {
-    this.name = 'Adam'
-  }
 
-  print() {
-    return this.name
-  }
+function Car() {
+  this.make = 'audi'
+  this.model = 'a4'
 }
 
-window.addEventListener('load', () => {
-  const pro = new Pro()
-  console.log(pro.print())
-})
+function Bmw() {
+  this.price = 20000
+}
+
+
+
+const car = new Car()
+
+// can only assign prototype to an instantiated object
+Bmw.prototype = car
+
+console.log(car)
+
+Car.prototype.honk = function () {
+  console.log('beep beep')
+}
+car.honk()
+
+const bmw = new Bmw()
+
+
+let user = {
+  name: "John",
+  surname: "Smith",
+
+  set fullName(value) {
+    [this.name, this.surname] = value.split(" ");
+  },
+
+  get fullName() {
+    return `${this.name} ${this.surname}`;
+  }
+};
+
+let admin = {
+  __proto__: user,
+  isAdmin: true
+};
+
+console.log(`%c ${admin.fullName}`, 'background: black; color: orange;'); // John Smith
+
+admin.fullName = "Alice Cooper"
+
+console.log(admin.fullName) // Alice Cooper
+console.log(user.fullName) // John Smith
