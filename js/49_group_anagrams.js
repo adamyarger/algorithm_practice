4cfabb9bd84b074df3ef4b20a2aba57fcf8069d3
Adam Yarger <yargeradam@gmail.com>
2021-07-15 10:22:38 -0500

    adds anagram

---
+++ js/49_group_anagrams.js
@@ -0,0 +1,55 @@
+/**
+ * 
+ * Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+
+
+Example 1:
+
+Input: strs = ["eat","tea","tan","ate","nat","bat"]
+Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+Example 2:
+
+Input: strs = [""]
+Output: [[""]]
+Example 3:
+
+Input: strs = ["a"]
+Output: [["a"]]
+ */
+
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ * 
+ * 
+ * group anan grams together
+ * 
+ * how do we find anangrams?
+ * could sort each word alphbetically and compare would be O(n log n)
+ * 
+ * search for anangams
+ * 
+ * could use backtracking type algo, find anangrams and take aways from candidates once added to cur
+ * 
+ * 
+ */
+var groupAnagrams = function (strs) {
+  const dict = {}
+
+  for (let str of strs) {
+    const key = str.split('').sort()
+
+    if (!(key in dict)) {
+      dict[key] = []
+    }
+
+    dict[key].push(str)
+  }
+
+  return Object.values(dict)
+};
+
+console.log(groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"]))
