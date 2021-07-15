7819d379fdc7087fd86f95c2636abeea9761c8b2
Adam Yarger <yargeradam@gmail.com>
2021-07-14 21:34:46 -0500

    stash

---
+++ js/virtual-scroll/index.js
@@ -0,0 +1,33 @@
+/**
+ *  could turn this int a headles virtual scroll react package that can be used for gallerys or tables
+ *
+ * sentinal element (how does google do it?)
+ *
+ * buffer for top and bottom
+ * page height so we know how many photots to load
+ * 
+ * Page function adding and removing whole pages
+ *
+ * on Resize
+ *
+ * onscroll
+ *
+ *keep a shadow root for creating new elements that will get mounte next
+ *
+ */
+
+
+(function Scroller(window) {
+
+
+
+  function Page() {
+
+  }
+
+  function Item() {
+
+  }
+
+
+})(window)
