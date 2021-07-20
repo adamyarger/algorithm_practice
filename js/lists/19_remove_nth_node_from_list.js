6ff33bc58e77e1096e7d8dcb4d37fcab7dae4637
Adam Yarger <yargeradam@gmail.com>
2021-07-20 12:47:09 -0500

    first pass wokrs

---
+++ js/lists/19_remove_nth_node_from_list.js
@@ -0,0 +1,86 @@
+/**
+ * 
+ * Given the head of a linked list, remove the nth node from the end of the list and return its head.
+
+
+
+Example 1:
+
+
+Input: head = [1,2,3,4,5], n = 2
+Output: [1,2,3,5]
+Example 2:
+
+Input: head = [1], n = 1
+Output: []
+Example 3:
+
+Input: head = [1,2], n = 1
+Output: [1]
+ */
+
+
+
+function ListNode(val, next) {
+  this.val = (val === undefined ? 0 : val)
+  this.next = (next === undefined ? null : next)
+}
+
+/**
+ * @param {ListNode} head
+ * @param {number} n
+ * @return {ListNode}
+ * 
+ * this is nth from the end
+ * could do fast and slow pointer, that way on hit the end while the other is in the middle and knows where to go to
+ * 
+ * step 1: find the nth to last node
+ * step 2: remove node
+ * - track prev
+ * - point prev to cur.next
+ * 
+ * 
+ * IDEA: move first poiter forward by n amount
+ * then add a second pointer at the beginning and move forward until the first one hits the end
+ * by that time the newer index will be in place
+ */
+var removeNthFromEnd = function (head, n) {
+  let first = head
+
+  for (let i = 0; i < n; i++) {
+    first = first.next
+  }
+
+  let second = head
+  let prev = null
+
+  while (first) {
+    first = first.next
+    prev = second
+    second = second.next
+  }
+  // its aat target since it goes one more time when the first hits null
+  // need to keep a prev
+  console.log(prev.val)
+  // after the cur node will be our prev to the target node
+  // should be 3
+  // unlink the target
+  const _next = prev.next.next
+  prev.next.next = null
+  prev.next = _next
+  return head
+};
+
+let head = new ListNode(1)
+head.next = new ListNode(2)
+head.next.next = new ListNode(3)
+head.next.next.next = new ListNode(4)
+head.next.next.next.next = new ListNode(5)
+
+let node = removeNthFromEnd(head, 2)
+
+console.log('-----')
+while (node) {
+  console.log(node.val)
+  node = node.next
+}
