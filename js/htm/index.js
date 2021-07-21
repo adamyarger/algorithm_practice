0abf0547cbd744c9ea8c482cf00d2d5c50ee4a4c
Adam Yarger <yargeradam@gmail.com>
2021-07-21 10:02:42 -0500

    stash htm

---
+++ js/htm/index.js
@@ -0,0 +1,41 @@
+
+const htm = (function () {
+  const MODE_SLASH = 0;
+  const MODE_TEXT = 1;
+  const MODE_WHITESPACE = 2;
+  const MODE_TAGNAME = 3;
+  const MODE_COMMENT = 4;
+  const MODE_PROP_SET = 5;
+  const MODE_PROP_APPEND = 6;
+
+  const CHILD_APPEND = 0;
+  const CHILD_RECURSE = 2;
+  const TAG_SET = 3;
+  const PROPS_ASSIGN = 4;
+  const PROP_SET = MODE_PROP_SET;
+  const PROP_APPEND = MODE_PROP_APPEND;
+
+  const evaluate = (h, built, fields, args) => {
+
+  }
+
+  // string literals get passed in here
+  const build = function (statics) {
+    const fields = arguments
+    const h = this // might have to bind this
+
+    let mode = MODE_TEXT
+    let buffer = ''
+    let quote = ''
+    let current = [0]
+    let char, propName
+
+    const commit = field => {
+
+    }
+
+    for (let i = 0; i < statics.length; i++) {
+      // if
+    }
+  }
+})()
