c1550b422806b5197bd17f361be93b1c732f5f2b
Adam Yarger <yargeradam@gmail.com>
2021-07-23 19:56:32 -0500

    better notes

--- js/array/334_increasing_triplet_subsequence.js
+++ js/array/334_increasing_triplet_subsequence.js
@@ -24,38 +24,27 @@ Explanation: The triplet (3, 4, 5) is valid because nums[3] == 0 < nums[4] == 4 
  * @param {number[]} nums
  * @return {boolean}
  * 
- * must be in order from left to right and number must be in increasing order like 1,2,3
- * does something like 1,0,2,3 also work? can there be other numbers in between?
- * or does it have to be contigous? NO it does look like it
  * 
- * could treat it like 3sum
- * start with left mid and right pointers
- * 
- * use first item as partition then left = i + 1 and right
- * mve mid and test each then move left forward and repeat
- * 
- * reason why? because index matters so left mid and right matters
- * 
- * will be around O(n^2)
+ * its not about pointers and indexes its about values
+ * one constraints is order of indexes which just means move linearly
+ * which mean if we find one that bigger then nother bigger than that one then another bigger than that we win
  */
 var increasingTriplet = function (nums) {
-  let min = nums[0]
-  // the mid value which must be bigger than min and smaller than end
-  let secondMinUpdatedAfterMin = Infinity
+  let lo = nums[0]
+  let mid = Infinity
 
-  for (let cur of nums) {
-    // wil compare current and prev
-    // if cur is smaller than the min min will no longer work
-    if (cur <= min) {
-      min = cur
-    } else if (cur <= secondMinUpdatedAfterMin) {
-      // second min is our middle value it need to be 
-      // if we reach this statement it means min is less than cur so now we need cur to be greater than mid
-      secondMinUpdatedAfterMin = cur
+  for (cur of nums) {
+    if (cur <= lo) {
+      // set low to the lowest value we find
+      lo = cur
+    } else if (cur <= mid) {
+      mid = cur
     } else {
+      // this will return early
       return true
     }
   }
+
   return false
 };
 
