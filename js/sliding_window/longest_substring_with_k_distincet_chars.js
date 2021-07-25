a66ddcd1ba9e13ecd0529a9737b082a8b3c9619b
Adam Yarger <yargeradam@gmail.com>
2021-07-25 10:12:09 -0500

    adds longest substring distinct chars

--- js/sliding_window/longest_substring_with_k_distincet_chars.js
+++ js/sliding_window/longest_substring_with_k_distincet_chars.js
@@ -21,6 +21,49 @@ Output: 5
 Explanation: The longest substrings with no more than '3' distinct characters are "cbbeb" & "bbebi".
  */
 
+/**
+ * 
+ * @param {string} str 
+ * @param {number} k 
+ * 
+ * find the longest substring but your only allowed to use k different letters
+ * substring and contigous are give aways for sliding window
+ * 
+ * grow the window until the count of uniqu chars goes over
+ * how to track unique chars? map? or set?
+ * with a a set we can use length
+ * remove from set when left is moved
+ */
 var longest_substring_with_k_distinct = function (str, k) {
+  // need count so need dict
+  // could use set of lenth 26 to keep count then use reduce for sum
+  const used = {}
+  let left = 0
+  let count = 0
 
+  for (let right = 0; right < str.length; right++) {
+    const char = str[right]
+    used[char] = char in used ? used[char] + 1 : 1
+
+    // const chars = Object.keys(used)
+    while (Object.keys(used).length > k) {
+      // remove from left, could be repeating chars like aaaa
+      const val = str[left]
+      used[val]--
+      left++
+      if (used[val] === 0) {
+        delete used[val]
+      }
+    }
+
+    // check if new sum is larger
+    const sum = Object.values(used).reduce((acc, item) => acc + item, 0)
+    count = Math.max(sum, count)
+  }
+
+  return count
 }
+
+
+console.log(longest_substring_with_k_distinct('araaci', 2)) //araa
+console.log(longest_substring_with_k_distinct("cbbebi", 3))
