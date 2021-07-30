101441ff9aa1e8d130db14d694b2b0c27820ffbb
Adam Yarger <yargeradam@gmail.com>
2021-07-30 15:03:11 -0500

    adds meeting room

--- js/merge_intervals/253. Meeting Rooms II.js
+++ js/merge_intervals/253. Meeting Rooms II.js
@@ -11,10 +11,42 @@ Example 2:
 Input: [[7,10],[2,4]]
 
 Output:1
-Thoughts:
 
-Sort the interval by the start time
-Using the priority queue to use the end time as the order to sort the used classroom
-pop the earliest ending meeting room, check if the time ends earlier than the start time of current class being scheduled, if earlier, merge the interval by setting the poped intervals'end time as the current intervals' scheduled end time, push the current interval into pq as making a new room.
-return the size of the pq as the result
+sort
+
+how do we determine meeting rooms?
+how many meeting are happening at once?
  */
+
+var minMeetingRooms = function (intervals) {
+  let max = 0
+  let cur = 0
+
+  intervals.sort((a, b) => a[0] = b[0])
+
+  let start = intervals[0][0]
+  let end = intervals[0][1]
+
+  // find max overlaps at once
+  for (let i = 1; i < intervals.length; i++) {
+    const interval = intervals[i]
+
+    if (interval[0] < end) {
+      // overlap add to cur count
+      cur += 1
+    } else {
+      cur = 0
+    }
+
+    start = interval[0]
+    end = interval[1]
+
+    max = Math.max(max, cur)
+  }
+
+  return max
+}
+
+
+console.log(minMeetingRooms([[0, 30], [5, 10], [15, 20]])) // 2
+console.log(minMeetingRooms([[7, 10], [2, 4]])) // 1
