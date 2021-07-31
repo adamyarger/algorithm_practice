b447b0dc29b225e802a7b810658d9b1bdfa205d5
Adam Yarger <yargeradam@gmail.com>
2021-07-31 11:09:02 -0500

    build hap works

---
+++ js/utils/MinHeap.js
@@ -0,0 +1,98 @@
+
+class MinHeap {
+  constructor() {
+    // this makes getting parent easier
+    this.items = [0]
+  }
+
+  get last() {
+    return this.items.length - 1
+  }
+
+  // read the min value
+  peek() {
+    return this.last ? this.items[1] : undefined
+  }
+
+  // push to the end of items
+  // then trigger percolate on the item so it bubbles up till its parent is smaller than it
+  push(val) {
+    this.items.push(val)
+    this._swim()
+  }
+
+  _swim() {
+    let i = this.last
+
+    while (Math.floor(i / 2) > 0) {
+      const parent = Math.floor(i / 2)
+      if (this.items[i] < this.items[parent]) {
+        this._swap(i, parent)
+      }
+      i = parent
+    }
+  }
+
+  _swap(index1, index2) {
+    const temp = this.items[index1]
+    this.items[index1] = this.items[index2]
+    this.items[index2] = temp
+  }
+
+  // swap the first item with the last item
+  // save first item to a variable
+  // pop off the last item
+  // sink the first item
+  // by swapping we no longer trigger the array to resize all elements
+  pop() {
+    const val = this.items[1]
+    //swap with the last item
+    this._swap(1, this.len)
+    // get rid of last item which was the min val
+    this.items.pop()
+    // sink the last item that got put at the top
+    this._sink(1)
+    // return the min val
+    return val
+  }
+
+  _sink(i) {
+    // swap with 
+
+    while (i * 2 <= this.last) {
+      // al that matters is the min item that will replace it
+      const min_i = this.minChild(i)
+      if (this.items[i] > this.items[min_i]) {
+        this._swap(i, min_i)
+      }
+      i = min_i
+    }
+  }
+
+  // find the min child of a parent
+  minChild(i) {
+    let left = i * 2
+    let right = left + 1
+
+    // no right exists
+    if (right > this.last) {
+      return left
+    }
+    return this.items[left] < this.items[right] ? left : right
+  }
+
+  // create a heap from an array
+  heapify() {
+
+  }
+}
+
+const heap = new MinHeap()
+heap.push(5)
+heap.push(3)
+heap.push(2)
+heap.push(4)
+
+console.log(heap.items)
+console.log(heap.pop())
+console.log(heap.items)
