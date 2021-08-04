612c34ce13fa8c754653ec4719b375856a5f636e
Adam Yarger <yargeradam@gmail.com>
2021-08-04 18:57:30 -0500

    use double while

--- js/sliding_window/424_longest_repeating_char_replacement.js
+++ js/sliding_window/424_longest_repeating_char_replacement.js
@@ -1,10 +1,40 @@
 /**
+ * You are given a string s and an integer k. You can choose any character of the string and change it to any other uppercase English character. You can perform this operation at most k times.
+
+Return the length of the longest substring containing the same letter you can get after performing the above operations.
+
+
+
+Example 1:
+
+Input: s = "ABAB", k = 2
+Output: 4
+Explanation: Replace the two 'A's with two 'B's or vice versa.
+Example 2:
+
+Input: s = "AABABBA", k = 1
+Output: 4
+Explanation: Replace the one 'A' in the middle with 'B' and form "AABBBBA".
+The substring "BBBB" has the longest repeating letters, which is 4.
+ */
+
+/**
  * @param {string} s
  * @param {number} k
  * @return {number}
- * 
- * same as flip bits but with letters
- * longest repeating char with k wildcards
+ *
+ * return longest substring of the same letter
+ * can have up to k wildcard letters
+ *
+ * need to know the most frequest letter since its the most liekly winner
+ * but that means we need to track all letter lengths to know when frequency change
+ *
+ * going over moving left
+ *
+ * sliding window is almost always a nested while loop in a for loop
+ *
+ * MISSED point before:
+ * // how do we check going over k?
  */
 var characterReplacement = function (s, k) {
   let seen = {}
