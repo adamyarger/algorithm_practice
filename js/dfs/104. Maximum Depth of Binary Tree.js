0fefe3953a456e89276820d69733bf12b2a10690
Adam Yarger <yargeradam@gmail.com>
2021-08-05 11:17:36 -0500

    adds max length

---
+++ js/dfs/104. Maximum Depth of Binary Tree.js
@@ -0,0 +1,49 @@
+/**
+ * Given the root of a binary tree, return its maximum depth.
+
+A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+
+
+
+Example 1:
+
+
+Input: root = [3,9,20,null,null,15,7]
+Output: 3
+Example 2:
+
+Input: root = [1,null,2]
+Output: 2
+Example 3:
+
+Input: root = []
+Output: 0
+Example 4:
+
+Input: root = [0]
+Output: 1
+ */
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function (root) {
+  function dfs(node) {
+    if (!node) return 0
+
+    return Math.max(
+      dfs(node.left),
+      dfs(node.right)
+    ) + 1
+  }
+  return dfs(root)
+};
