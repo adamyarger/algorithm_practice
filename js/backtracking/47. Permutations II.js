8a18c6c47993caa00557a9398cbb00453c1c0f61
Adam Yarger <yargeradam@gmail.com>
2021-08-08 11:07:19 -0500

    adds permutations 2

--- js/backtracking/47. Permutations II.js
+++ js/backtracking/47. Permutations II.js
@@ -20,12 +20,40 @@ Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
 /**
  * @param {number[]} nums
  * @return {number[][]}
+ * 
+ * permutations with dups
  */
 var permuteUnique = function (nums) {
+  const out = []
 
+  nums.sort((a, b) => a - b)
+
+  function backtrack(nums, cur) {
+    // take away from nums
+    if (nums.length === 0) {
+      out.push(cur)
+      return
+    }
+
+    for (let i = 0; i < nums.length; i++) {
+      if (i && nums[i] === nums[i - 1]) continue
+
+      backtrack(
+        [...nums.slice(0, i), ...nums.slice(i + 1)],
+        cur.concat(nums[i])
+      )
+    }
+  }
+
+  backtrack(nums, [])
+  return out
 };
 
 
-test('foo', () => {
-  expect(true).toBe(true)
+console.log(permuteUnique([1, 1, 2]))
+
+test('should pass', () => {
+  expect(permuteUnique([1, 1, 2])).toEqual([[1, 1, 2],
+  [1, 2, 1],
+  [2, 1, 1]])
 })
