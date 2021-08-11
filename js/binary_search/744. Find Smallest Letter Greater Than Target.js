4aea447860eb7d9bff9ae60913f0f1d28c47b59f
Adam Yarger <yargeradam@gmail.com>
2021-08-11 09:01:10 -0500

    adds notes

---
+++ js/binary_search/744. Find Smallest Letter Greater Than Target.js
@@ -0,0 +1,33 @@
+/**
+ * @param {character[]} letters
+ * @param {character} target
+ * @return {character}
+ */
+var nextGreatestLetter = function (letters, target) {
+  // edge case if target is bigger than the biggest letter wrap around and return the smallest
+  if (target >= letters[letters.length - 1]) return letters[0]
+  let lo = 0;
+  let hi = letters.length - 1
+
+  // why not <= ??? 
+  // the equals in <= is meant to handle the case where lo and hi are the same index
+  // when lo and hi are the same index it means theres 1 index to search
+  // this only matters when we need to compare for exact value
+  // we dont care about exact value, we care about best aproximation so we can stop and just return that
+  // the final index left is the answer, which both lo and hi will be the last index at that point
+  while (lo < hi) {
+    let mid = Math.floor((hi + lo) / 2)
+
+    if (letters[mid] <= target) {
+      lo = mid + 1
+    } else {
+      hi = mid
+    }
+  }
+
+  return letters[hi]
+};
+
+console.log(nextGreatestLetter(["c", "f", "j"], 'k')) //c
+
+console.log(nextGreatestLetter(["c", "f", "j"], 'c')) // f
