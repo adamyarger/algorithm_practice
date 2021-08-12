de149001e3af05e3bf94a77b249372dc77b7cf80
Adam Yarger <yargeradam@gmail.com>
2021-08-12 11:19:59 -0500

    stash

--- js/binary_search/744. Find Smallest Letter Greater Than Target.js
+++ js/binary_search/744. Find Smallest Letter Greater Than Target.js
@@ -15,7 +15,7 @@ var nextGreatestLetter = function (letters, target) {
   // this only matters when we need to compare for exact value
   // we dont care about exact value, we care about best aproximation so we can stop and just return that
   // the final index left is the answer, which both lo and hi will be the last index at that point
-  while (lo < hi) {
+  while (lo <= hi) {
     let mid = Math.floor((hi + lo) / 2)
 
     // why <= on this one?
@@ -25,13 +25,43 @@ var nextGreatestLetter = function (letters, target) {
       // move lo up past mid to search the right half
       lo = mid + 1
     } else {
+      // why not mid-1
+      // it has somthing to do with <= target since those are usualy 2 seperate steps
+      hi = mid - 1
+    }
+  }
+
+  console.log(lo, hi)
+  return letters[hi]
+};
+
+// console.log(nextGreatestLetter(["c", "f", "j"], 'k')) //c
+
+console.log(nextGreatestLetter(["c", "f", "j"], 'c')) // f
+
+
+// the edge case is when the target === mid
+var option2 = function (letters, target) {
+  if (target >= letters[letters.length - 1]) return letters[0]
+  let lo = 0;
+  let hi = letters.length - 1
+
+  // why not <= ??? 
+  // the equals in <= is meant to handle the case where lo and hi are the same index
+  // when lo and hi are the same index it means theres 1 index to search
+  // this only matters when we need to compare for exact value
+  // we dont care about exact value, we care about best aproximation so we can stop and just return that
+  // the final index left is the answer, which both lo and hi will be the last index at that point
+  while (lo < hi) {
+    let mid = Math.floor((hi + lo) / 2)
+
+    if (letters[mid] <= target) {
+      lo = mid + 1
+    } else {
       hi = mid
     }
   }
 
+  console.log(lo, hi)
   return letters[hi]
 };
-
-console.log(nextGreatestLetter(["c", "f", "j"], 'k')) //c
-
-console.log(nextGreatestLetter(["c", "f", "j"], 'c')) // f
