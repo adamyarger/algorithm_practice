dc6f4dbc7f9deff907cc26721706c5d1aa340096
Adam Yarger <yargeradam@gmail.com>
2021-08-14 14:56:42 -0500

    stash

--- js/top_k/347. Top K Frequent Elements.js
+++ js/top_k/347. Top K Frequent Elements.js
@@ -52,11 +52,68 @@ var topKFrequent = function (nums, k) {
   })
 };
 
-console.log(topKFrequent([1, 1, 1, 2, 2, 3], 2))
+// console.log(topKFrequent([1, 1, 1, 2, 2, 3], 2))
+// console.log(topKFrequent([4, 1, -1, 2, -1, 2, 3], 2))
 
-console.log(topKFrequent([4, 1, -1, 2, -1, 2, 3], 2))
 
-
-// bucket sort
 
 // quick select
+function quick(nums, k) {
+  const map = {}
+
+  nums.forEach(item => {
+    map[item] = map[item] ? map[item] + 1 : 1
+  })
+
+  console.log(map)
+
+  const counts = Object.entries(map)
+
+  return partition(counts, 0, counts.length - 1, k).map(item => item[0])
+}
+
+/**
+ * 
+ * goal is to get every thing on the left lower than the pivot and everything on the right higher than the pivot
+ * once left and right are the same index we swap with pivot to make it true
+ * https://leetcode.com/problems/kth-largest-element-in-an-array/discuss/664455/JavaScript-Iterative-Quick-Select-O(N)-Heavily-Commented
+ */
+function partition(counts, lo, hi, k) {
+  const pivot = counts[lo][1]
+  let left = lo + 1
+  let right = hi
+
+  while (left < right) {
+    while (left < right && counts[left][1] <= pivot) {
+      left += 1
+    }
+
+    while (right > left && counts[right][1] >= pivot) {
+      right -= 1
+    }
+
+    // console.log(counts[left])
+
+    [counts[left], counts[right]] = [counts[right], counts[left]]
+  }
+  [counts[lo], counts[left]] = [counts[left], counts[lo]]
+
+  console.log(counts)
+
+  // if enough values on he left are in place we should be good
+  if (left + 1 === k) {
+    return counts.slice(0, left + 1)
+  } else if (left + 1 < k) {
+    return partition(left + 1, hi)
+  } else {
+    return partition(lo, left - 1)
+  }
+}
+
+function swap(a, b) {
+  [counts[a], counts[b]] = [counts[b], counts[a]]
+}
+
+console.log(quick([4, 1, -1, 2, -1, 2, 3], 2))
+
+// console.log(quick([2, 3, 1, 8, 3, 5, 2, 6], 2))
