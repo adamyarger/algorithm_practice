1c9c6febf6d2fa2c5d22860472c173230d50d2ab
Adam Yarger <yargeradam@gmail.com>
2021-08-19 10:19:45 -0500

    adds quick select kth largest

--- js/top_k/215. Kth Largest Element in an Array.js
+++ js/top_k/215. Kth Largest Element in an Array.js
@@ -46,6 +46,53 @@ var findKthLargest = function (nums, k) {
 };
 
 
-console.log(findKthLargest([3, 2, 1, 5, 6, 4], 2))
+// console.log(findKthLargest([3, 2, 1, 5, 6, 4], 2))
 // [3, 2, 1, 5, 6, 4]
 // [1, 2, 3, 4, 5, 6]
+
+
+{
+  let findKthLargest = function (nums, k) {
+    return partition(nums, 0, nums.length - 1, nums.length - k)
+  };
+
+  function partition(nums, lo, hi, k) {
+    function swap(a, b) {
+      // descructuring creates variables under the hood
+      // so a and b vars are created then reassined, works similar to temp cache way
+      [nums[a], nums[b]] = [nums[b], nums[a]]
+    }
+
+    let pivot = nums[lo]
+    let left = lo + 1
+    let right = hi
+
+    while (true) {
+      while (left <= right && nums[left] <= pivot) {
+        left += 1
+      }
+
+      while (left <= right && nums[right] >= pivot) {
+        right -= 1
+      }
+
+      if (right < left) {
+        break
+      }
+
+      swap(left, right)
+    }
+    swap(lo, right)
+
+    if (right === k) {
+      return nums[right]
+    } else if (right > k) {
+      // search left side
+      return partition(nums, lo, right - 1, k)
+    } else {
+      return partition(nums, right + 1, hi, k)
+    }
+  }
+
+  console.log(findKthLargest([3, 2, 3, 1, 2, 4, 5, 5, 6], 4))
+}
