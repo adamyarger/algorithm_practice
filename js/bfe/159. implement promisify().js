b35306ffa5d4033891cc2211153c29d3a6dde42f
Adam Yarger <yargeradam@gmail.com>
2021-08-24 13:18:33 -0500

    adds notes on promisify

--- js/bfe/159. implement promisify().js
+++ js/bfe/159. implement promisify().js
@@ -42,6 +42,10 @@ function promisify(func) {
   return function (...args) {
     return new Promise((resolve, reject) => {
       // fill in the arbitrary args then leave the callback o be handled and turned into a promise
+      // this IS NOT!!! have anything to do with scope, its about what this is during the execution context
+      // this matter when promisify is used inside an object that gets called in dot notation
+
+      // were declaring the callback that gets passed to the func
       func.call(...args, (error, data) => {
         if (error) {
           reject(error)
@@ -53,15 +57,14 @@ function promisify(func) {
   }
 }
 
-
-
-const callback = (error, data) => {
-  if (error) {
-    // handle the error
-  } else {
-    // handle the data
-  }
-}
+// this is an example its not actually used
+// const callback = (error, data) => {
+//   if (error) {
+//     // handle the error
+//   } else {
+//     // handle the data
+//   }
+// }
 
 const func = (arg1, arg2, callback) => {
   // some async logic
@@ -74,7 +77,7 @@ const func = (arg1, arg2, callback) => {
 
 const promisedFunc = promisify(func)
 
-promisedFunc().then((data) => {
+promisedFunc(1, 2).then((data) => {
   // handles data
 }).catch((error) => {
   // handles error
