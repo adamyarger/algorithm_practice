07d8a5e4125911c96e379091db75499310949e88
Adam Yarger <yargeradam@gmail.com>
2021-08-24 13:59:12 -0500

    add short way to do curry

--- js/bfe/curry.js
+++ js/bfe/curry.js
@@ -19,25 +19,22 @@ function naive(fn) {
   }
 }
 
-// it doesnt matter if its called ...args, it only matters that its the last argumnt and its using a spread operator
-function curry(fn) {
-  // use ...args since we dont know how many args will be passed in
-  // arguments sets activation object in the execution context
+function vcurry(fn) {
+  // we dont know the args count
+  // it needs to be named so we can call it recursivly
   return function curried(...args) {
-    // fn.length return snumber of params expected
     if (args.length >= fn.length) {
-      // this is the base case, were always going to end by calling it as a function with arguments as array
-      // the purpose of the other if is to keep adding arguments to the array
-      // this is still global. were just using it to fire a function with arguments as an array
       return fn.apply(this, args)
     } else {
-      // use spread operator since its could be foo(1,2)
       return function (...args2) {
-        // keep adding args to the args array until we have enough to fire the function like normal
         return curried.apply(this, args.concat(args2))
       }
     }
   }
+}
+
+const curry = fn => function curried(...args) {
+  return args.length >= fn.length ? fn(...args) : (...args2) => curried(...args, ...args2)
 }
 
 
