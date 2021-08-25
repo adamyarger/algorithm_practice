87ee7ef79515a522bf31a6fc98bf3130fce091c0
Adam Yarger <yargeradam@gmail.com>
2021-08-25 09:16:55 -0500

    adds once

--- js/bfe/46. implement `_.once()`.js
+++ js/bfe/46. implement `_.once()`.js
@@ -1,7 +1,22 @@
 
+/**
+ * 
+ * @param {*} fn 
+ * 
+ * only calculate the value once, then return the cached value
+ */
+function once(func) {
+  let count = 0
+  let val
 
-function once(fn) {
+  return function (...args) {
+    if (!count) {
+      val = func.apply(this, args)
+      count += 1
+    }
 
+    return val
+  }
 }
 
 function func(num) {
@@ -10,8 +25,8 @@ function func(num) {
 
 const onced = once(func)
 
-onced(1)
+console.log(onced(13))
 // 1, func called with 1
 
-onced(2)
+console.log(onced(2))
 // 1, even 2 is passed, previous result is returned
