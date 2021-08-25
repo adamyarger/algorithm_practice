9f1f3fd8dde7114bd7635b8c81fe46dfeef45cc4
Adam Yarger <yargeradam@gmail.com>
2021-08-25 08:15:00 -0500

    stash

--- js/bind.js
+++ js/bind.js
@@ -15,3 +15,30 @@ const foo = hardBind(function (key) {
 }, obj)
 
 foo('dude')
+
+
+function MyObject(arg) {
+  this.testNumber = arg
+}
+
+function MyObject2(arg) {
+  this.testString = arg
+}
+
+// replace the default prototype of MyObject2
+// prototype is only avaiable for assignment else its a hidden object represented as [[prototype]]
+MyObject2.prototype = new MyObject(8)
+
+// create instance of MyObj2
+let objectRef = new MyObject2('string val')
+
+/**
+ * the prototype for MyObject2 will be MyObject
+ *
+ * objRef = {
+ *  testString: 'string val'
+ *  __proto__: {
+ *    testNumber: 8
+ *  }
+ * }
+ */
