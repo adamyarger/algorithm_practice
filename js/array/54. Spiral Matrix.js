84501a2e31b421de123bf61c65f0215a5d1eb2bb
Adam Yarger <yargeradam@gmail.com>
2021-08-26 12:29:27 -0500

    adds notes

--- js/array/54. Spiral Matrix.js
+++ js/array/54. Spiral Matrix.js
@@ -17,20 +17,29 @@ Output: [1,2,3,4,8,12,11,10,9,5,6,7]
 
 var spiralOrder = function (matrix) {
   const res = []
+  // move towards base case by popping off
   while (matrix.length) {
     const first = matrix.shift()
     // why spread? becuase first is an array
-    res.push(first)
+    res.push(...first)
+
+    // [ 
+    // [ 4, 5, 6 ], 
+    // [ 7, 8, 9 ] ] is left
 
     for (const m of matrix) {
       let val = m.pop()
       if (val) {
         res.push(val)
         m.reverse()
+        // 5,4
+        // 8,7
       }
     }
 
     matrix.reverse()
+    // 8,7
+    // 5,4
   }
   return res
 }
@@ -38,3 +47,4 @@ var spiralOrder = function (matrix) {
 
 let matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 console.log(spiralOrder(matrix))
+//Output: [1,2,3,6,9,8,7,4,5]
