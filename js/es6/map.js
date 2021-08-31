15f35eec26aac7b09a8790af44df55531a071017
Adam Yarger <yargeradam@gmail.com>
2021-08-31 13:00:33 -0500

    adds wekset exmaple

--- js/es6/map.js
+++ js/es6/map.js
@@ -1,19 +1,63 @@
 
 
-class ISet extends Set {
-  map(fn) {
-    const out = new Set()
-    this.forEach(item => {
-      out.add(fn(item))
-    })
-    return out
+{
+  class ISet extends Set {
+    map(fn) {
+      const out = new Set()
+      this.forEach(item => {
+        out.add(fn(item))
+      })
+      return out
+    }
   }
+
+  let set = new ISet([1, 2, 3, 4])
+
+  console.log(set)
+
+  let sum = set.map(item => item * 2)
+
+  console.log(sum)
 }
 
-let set = new ISet([1, 2, 3, 4])
+{
+  // set keeps reference event when deleted
+  let set = new Set()
+  let key = { a: 23 }
 
-console.log(set)
+  // add reference to key object
+  set.add(key)
+  // console.log(set)
 
-let sum = set.map(item => item * 2)
+  // set key to null, no more reference
+  key = null
 
-console.log(sum)
+  // size is still 1 event though refeence no longer exists
+  console.log(set)
+
+  // we can get the reference back
+  key = [...set][0]
+
+  // console.log(key)
+}
+
+{
+  // WeakSet DOM node use case
+
+  // let set = new Set()
+  let weak = new WeakSet()
+
+  // set.add(document.querySelector('.one'))
+  weak.add(document.querySelector('.one'))
+
+  console.log('weak', weak.has(document.querySelector('.one'))) // true
+
+  document.querySelector('.one').remove()
+
+  // console.log('set', set)
+  console.log('weak', weak.has(document.querySelector('.one'))) // false, it got delete
+
+  // it doesnt exist, but set still references it
+  // if we use WekSet it wont
+  // console.log(document.querySelector('.one'))
+}
