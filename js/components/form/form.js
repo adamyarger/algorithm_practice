f2f4c7ac938e5c502d8b61b9790c96417501df3d
Adam Yarger <yargeradam@gmail.com>
2021-09-13 12:54:11 -0500

    form validaiton works

---
+++ js/components/form/form.js
@@ -0,0 +1,137 @@
+
+
+(function () {
+  const template = document.createElement('template')
+  template.innerHTML = `
+    <style>
+      .errors {
+        color: red;
+      }
+    </style>
+
+    <form class="form" method="post">
+      <div class="form-control">
+        <label for="name">Name</label>
+        <input class="input" type="text" name="name" required>
+      </div>
+
+      <div class="form-control">
+        <label for="email">Email</label>
+        <input class="input" type="email" name="email" required>
+      </div>
+
+      <div class="form-control">
+        <label for="password">Password</label>
+        <input class="input" type="password" name="password" required>
+      </div>
+
+      <div class="form-control">
+        <label for="confirm">Confirm Password</label>
+        <input class="input" type="password" name="confirm" required>
+      </div>
+
+      <input type="submit" value="Submit">
+    </form>
+
+    <ul class="errors">
+    </ul>
+  `
+
+  customElements.define('x-form', class XForm extends HTMLElement {
+    constructor() {
+      super()
+      this.attachShadow({ mode: 'open' })
+      this.shadowRoot.appendChild(template.content.cloneNode(true))
+      this.form = this.shadowRoot.querySelector('form')
+      this.submitBtn = this.shadowRoot.querySelector('[type="submit"]')
+      this.errorsEl = this.shadowRoot.querySelector('.errors')
+      this.inputs = Array.from(this.shadowRoot.querySelectorAll('.input'))
+
+      this._form = {
+        name: {
+          el: this.shadowRoot.querySelector('[name="name"]'),
+          valid() {
+            return this.el.value.length > 0
+          },
+          message: 'Name missing'
+        },
+        email: {
+          el: this.shadowRoot.querySelector('[name="email"]'),
+          valid: () => {
+            return !!this.shadowRoot.querySelector('[name="email"]:valid')
+          },
+          message: 'Invalid Email'
+        },
+        password: {
+          el: this.shadowRoot.querySelector('[name="password"]'),
+          valid() {
+            return this.el.value.length >= 6
+          },
+          message: 'Password too short'
+        },
+        confirm: {
+          el: this.shadowRoot.querySelector('[name="confirm"]'),
+          valid: () => {
+            return this._form.confirm.el.value === this._form.password.el.value
+              && this._form.password.valid()
+          },
+          message: 'Invalid match'
+        }
+      }
+    }
+
+    // validate on blur
+    connectedCallback() {
+
+
+      this.submitBtn.addEventListener('click', this.onSubmit.bind(this))
+    }
+
+    onSubmit(event) {
+      event.preventDefault()
+      const valid = this.validate()
+
+      console.log(new FormData(this.form))
+
+      if (valid) {
+        console.log(this.form)
+        new Event('submit', new FormData(this.form))
+      }
+    }
+
+    validate() {
+      const errors = []
+
+      for (const key in this._form) {
+        if (!this._form[key].valid()) {
+          errors.push(this._form[key])
+        }
+      }
+
+      if (errors.length) {
+        this.showErrors(errors)
+      } else {
+        this.errorsEl.style.display = 'none'
+      }
+
+      return errors.length === 0
+    }
+
+    showErrors(errors) {
+      let html = ''
+      this.errorsEl.getElementsByClassName.display = 'block'
+      errors.forEach(obj => {
+        html += `
+          <li>
+            ${obj.message}
+          </li>
+        `
+      })
+      this.errorsEl.innerHTML = html
+    }
+
+    getValidInputs() {
+      return Array.from(this.shadowRoot.querySelectorAll('.input:valid'))
+    }
+  })
+})()
