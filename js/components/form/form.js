c02626e08d8ed2bd9958906d401775977b455669
Adam Yarger <yargeradam@gmail.com>
2021-09-13 13:16:31 -0500

    shorter alidation

--- js/components/form/form.js
+++ js/components/form/form.js
@@ -9,7 +9,7 @@
       }
     </style>
 
-    <form class="form" method="post">
+    <form id="signup" method="post">
       <div class="form-control">
         <label for="name">Name</label>
         <input id="name" class="input" type="text" name="name" required>
@@ -30,7 +30,7 @@
         <input id="confirm" class="input" type="password" name="confirm" required>
       </div>
 
-      <button type="submit" id="submit">
+      <button type="submit" id="submit" disabled>
         Submit
       </button>
     </form>
@@ -44,102 +44,60 @@
       super()
       this.attachShadow({ mode: 'open' })
       this.shadowRoot.appendChild(template.content.cloneNode(true))
-      this.form = this.shadowRoot.querySelector('form')
+
+      this.form = this.shadowRoot.querySelector('#signup')
       this.submitBtn = this.shadowRoot.querySelector('#submit')
       this.errorsEl = this.shadowRoot.querySelector('.errors')
-      this.inputs = Array.from(this.shadowRoot.querySelectorAll('.input'))
-
-      this._form = {
-        name: {
-          el: this.shadowRoot.querySelector('[name="name"]'),
-          valid() {
-            return this.el.value.length > 0
-          },
-          message: 'Name missing'
-        },
-        email: {
-          el: this.shadowRoot.querySelector('[name="email"]'),
-          valid: () => {
-            return !!this.shadowRoot.querySelector('[name="email"]:valid')
-          },
-          message: 'Invalid Email'
-        },
-        password: {
-          el: this.shadowRoot.querySelector('[name="password"]'),
-          valid() {
-            return this.el.value.length >= 6
-          },
-          message: 'Password too short'
-        },
-        confirm: {
-          el: this.shadowRoot.querySelector('[name="confirm"]'),
-          valid: () => {
-            return this._form.confirm.el.value === this._form.password.el.value
-              && this._form.password.valid()
-          },
-          message: 'Invalid match'
-        }
-      }
     }
 
-    // validate on blur
     connectedCallback() {
-
-
-      this.submitBtn.addEventListener('click', this.onSubmit.bind(this))
-
       this.form.addEventListener('change', this.onFormChange.bind(this))
     }
 
-    onFormChange(event) {
-      console.log(event)
+    disconnectedCallback() {
+      this.form.removeEventListener('change', this.onFormChange.bind(this))
     }
 
-    onSubmit(event) {
-      event.preventDefault()
-      const valid = this.validate()
-
-      console.log(new FormData(this.form))
-
-      if (valid) {
-        console.log(this.form)
-        new Event('submit', new FormData(this.form))
-      }
+    onFormChange(event) {
+      this.validate()
     }
 
     validate() {
       const errors = []
 
-      for (const key in this._form) {
-        if (!this._form[key].valid()) {
-          errors.push(this._form[key])
-        }
+      if (!this.form.name.value) {
+        errors.push('Missing Name')
       }
 
-      if (errors.length) {
-        this.showErrors(errors)
-      } else {
-        this.errorsEl.style.display = 'none'
+      if (!this.form.querySelector('#email:valid')) {
+        errors.push('Invalid Email')
       }
 
-      return errors.length === 0
+      if (this.form.password.value.length < 6) {
+        errors.push('Password too short')
+      }
+
+      if (this.form.password.value !== this.form.confirm.value) {
+        errors.push('Mismatch Password')
+      }
+
+      this.renderErrors(errors)
     }
 
-    showErrors(errors) {
+    renderErrors(errors) {
+      if (!errors.length) {
+        this.submitBtn.removeAttribute('disabled')
+        this.errorsEl.getElementsByClassName.display = 'none'
+        return
+      }
+
       let html = ''
       this.errorsEl.getElementsByClassName.display = 'block'
-      errors.forEach(obj => {
-        html += `
-          <li>
-            ${obj.message}
-          </li>
-        `
+      errors.forEach(msg => {
+        html += `<li>${msg}</li>`
       })
       this.errorsEl.innerHTML = html
-    }
-
-    getValidInputs() {
-      return Array.from(this.shadowRoot.querySelectorAll('.input:valid'))
+      this.submitBtn.setAttribute('disabled', '')
     }
   })
 })()
