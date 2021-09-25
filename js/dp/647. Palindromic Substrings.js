7dee829cd5517e947ad8729221b6fbbc53b5f62d
Adam Yarger <yargeradam@gmail.com>
2021-09-25 09:06:27 -0600

    adds count palindromes

---
+++ js/dp/647. Palindromic Substrings.js
@@ -0,0 +1,50 @@
+/**
+ * Given a string s, return the number of palindromic substrings in it.
+
+A string is a palindrome when it reads the same backward as forward.
+
+A substring is a contiguous sequence of characters within the string.
+
+
+
+Example 1:
+
+Input: s = "abc"
+Output: 3
+Explanation: Three palindromic strings: "a", "b", "c".
+Example 2:
+
+Input: s = "aaa"
+Output: 6
+Explanation: Six palindromic strings: "a", "a", "a", "aa", "aa", "aaa".
+ */
+
+
+/**
+ * @param {string} s
+ * @return {number}
+ 
+ - count number of palindromes
+ - one letter counts
+ - use dfs
+ */
+var countSubstrings = function (s) {
+  let count = 0
+
+  for (let i = 0; i < s.length; i++) {
+    expand(i, i)
+    expand(i, i + 1)
+  }
+
+  function expand(left, right) {
+    while (s[left] === s[right] && left >= 0 && right < s.length) {
+      count += 1
+      left -= 1
+      right += 1
+    }
+  }
+
+  return count
+};
+
+console.log(countSubstrings('abba'))
