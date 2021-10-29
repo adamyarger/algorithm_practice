40d97ecd5b82b6be1d3ed80ad26668db3412222c
Adam Yarger <yargeradam@gmail.com>
2021-10-28 17:48:23 -0700

    rename variables

--- js/backtracking/282. Expression Add Operators.js
+++ js/backtracking/282. Expression Add Operators.js
@@ -3,8 +3,8 @@
 var addOperators = function (num, target) {
   const out = []
 
-  function backtrack(str, arr, sum, prev) {
-    if (!str.length && sum === target) out.push(arr.join(''))
+  function backtrack(str, cur, sum, prev) {
+    if (!str.length && sum === target) out.push(cur.join(''))
 
     let len = str.length
     // loop will run once
@@ -12,26 +12,26 @@ var addOperators = function (num, target) {
 
     // why start at 1. its for the slice
     for (let i = 1; i <= len; i++) {
-      // cur is the different numbers wee trying. e.g. 1 or 12 or 123
-      const cur = parseInt(str.slice(0, i))
+      // num is the different numbers wee trying. e.g. 1 or 12 or 123
+      const num = parseInt(str.slice(0, i))
 
       // rest is the rest its what still needs to be dealt with nxt iteration. this is the backtacking.
       const rest = str.slice(i)
 
-      if (!arr.length) {
-        // if arr is empty then add the current number.
-        backtrack(rest, [cur], cur, cur)
+      if (!cur.length) {
+        // if cur is empty then add the numrent number.
+        backtrack(rest, [num], num, num)
       } else {
-        backtrack(rest, arr.concat('+', cur), sum + cur, cur)
-        // why 0 - cur??? incase a unary -5 ??? it for the prev
-        backtrack(rest, arr.concat('-', cur), sum - cur, 0 - cur)
+        backtrack(rest, cur.concat('+', num), sum + num, num)
+        // why 0 - num??? incase a unary -5 ??? it for the prev
+        backtrack(rest, cur.concat('-', num), sum - num, 0 - num)
 
         // why??? must be the order of operations problem.
         // 12 * 4 = 48 ---> 10 + (2*4) = 18
         // we need the prev value 2 so we can multiply it
-        const prod = prev * cur // this is the (2*4)
+        const prod = prev * num // this is the (2*4)
         // we do sum - prev to offset what was added before
-        backtrack(rest, arr.concat('*', cur), sum - prev + prod, prod)
+        backtrack(rest, cur.concat('*', num), sum - prev + prod, prod)
       }
     }
   }
